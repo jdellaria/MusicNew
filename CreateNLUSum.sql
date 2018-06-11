20617c07a5bcb464d587a9f11b5240375d19a5e0
Jon Dellaria <jdellaria@gmail.com>
2018-06-11 19:43:04 -0400

    Delete CreateNLUSum.sql

--- CreateNLUSum.sql
+++
@@ -1,3 +0,0 @@
-
-MySQL5Dialect
-create table NLUWatson.Nlusum (Id BIGINT auto_increment PRIMARY KEY) SELECT year, item, itemtext, SUM(count) as count, AVG(anger) as anger, AVG(disgust)as disgust, AVG(fear) as fear, AVG(joy) as joy, AVG(relevance) as relevance, AVG(sadness) as sadness, AVG(sentiment) as sentiment, type FROM NLUWatson.Nlu group by year, item, itemtext, type;
