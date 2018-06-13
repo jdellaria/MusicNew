d7ab8d03eecfda1916bfee32a400fc9ddadb685f
Jon Dellaria <jdellaria@gmail.com>
2018-06-13 15:14:15 -0400

    Update ZeroTImeSongs.sql

--- ZeroTImeSongs.sql
+++ ZeroTImeSongs.sql
@@ -6,13 +6,14 @@ drop table if exists Music.ZeroTimeSongs;
 SET sql_mode = '';
 
 /* Create table that takes each item in the playq and determintes the amount of time the song actually played. */
-
-
-create table Music.DiffTime(Idy BIGINT auto_increment PRIMARY KEY) SELECT id,songID, TIMESTAMPDIFF(second, BeginPlayTime, EndPlayTime ) as diff, Music.songlibrary.Name, Music.songlibrary.Artist, Music.songlibrary.Album, Music.playq.BeginPlayTime from Music.playq  inner join Music.songlibrary on Music.playq.songID = Music.songlibrary.SongIndex;
+create table Music.DiffTime(Idy BIGINT auto_increment PRIMARY KEY) SELECT id,songID, TIMESTAMPDIFF(second, BeginPlayTime, EndPlayTime ) as diff, Music.songlibrary.Name, Music.songlibrary.Artist, Music.songlibrary.Album, Music.playq.BeginPlayTime, Music.songlibrary.Location from Music.playq  inner join Music.songlibrary on Music.playq.songID = Music.songlibrary.SongIndex;
 
 /* Create table that counts the number of times that the song actually had a song play time of 0 seconds. */
-create table Music.ZeroTimeCount (Idy BIGINT auto_increment PRIMARY KEY) SELECT songID, COUNT(songID) songcount FROM Music.DiffTime  where diff = 0 GROUP BY songID;
+create table Music.ZeroTimeCount (Idy BIGINT auto_increment PRIMARY KEY) SELECT songID, COUNT(*) songcount FROM Music.DiffTime  where diff = 0 GROUP BY songID;
 
+/* Create table that contains all the songs that were tried to be play and have a zero second play time */
+create table Music.ZeroTimeSongs(Idy BIGINT auto_increment PRIMARY KEY) SELECT Music.DiffTime.songID, Music.songlibrary.Name, Music.songlibrary.Artist, Music.songlibrary.Album, Music.songlibrary.Location, Music.DiffTime.BeginPlayTime from Music.DiffTime inner join Music.songlibrary on Music.DiffTime.songID = Music.songlibrary.SongIndex where Music.DiffTime.diff = 0 ;
 
-
-create table Music.ZeroTimeSongs(Idy BIGINT auto_increment PRIMARY KEY) SELECT Music.ZeroTimeCount.songID, Music.ZeroTimeCount.songcount, Music.songlibrary.Name, Music.songlibrary.Artist, Music.songlibrary.Album, Music.songlibrary.Location from Music.ZeroTimeCount  inner join Music.songlibrary on Music.ZeroTimeCount.songID = Music.songlibrary.SongIndex;
+/* These 2 querys should return the same results*/
+SELECT * FROM Music.DiffTime where diff = 0 order by BeginPlayTime desc;
+SELECT * FROM Music.ZeroTimeSongs  order by BeginPlayTime desc;
